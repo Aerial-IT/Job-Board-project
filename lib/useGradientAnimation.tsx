8787f7611ab97039b2a846f9813ce5e098687a01
Guven2001 <139533207+Guven2001@users.noreply.github.com>
2026-03-12 12:01:27 +0530

    Change the bgimage color in lib/useGradientAnimation.tsx

--- lib/useGradientAnimation.tsx
+++ lib/useGradientAnimation.tsx
@@ -7,7 +7,8 @@ export const useGradientAnimation = () => {
   useEffect(() => {
     setGradientStyle({
       backgroundSize: "400% 400%",
-      backgroundImage: "linear-gradient(-45deg, #ff3d00, #0400ff, #4dd0e1, #9c27b0)",
+      //backgroundImage: "linear-gradient(-45deg, #ff3d00, #0400ff, #4dd0e1, #9c27b0)",
+      backgroundImage: "linear-gradient(-45deg, #ADD8E6, #4169E1, #6495ED, #87CEFA)",
       animation: "gradientBackground 15s ease infinite",
     })
   }, [])
