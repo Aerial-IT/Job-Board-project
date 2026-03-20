fa5e94b578fbf5b769942dc9cf2970328c0e8973
Guven2001 <139533207+Guven2001@users.noreply.github.com>
2026-03-20 11:05:56 +0530

    Header.tsx file modified

--- components/landingPageComponent/Header.tsx
+++ components/landingPageComponent/Header.tsx
@@ -10,10 +10,10 @@ export function Header() {
   const router =useRouter()
   return (
     <header className="fixed  top-0 left-0 right-0 z-50 bg-black bg-opacity-20 backdrop-blur-md">
-      <div className="  max-w-7xl  mx-auto px-4 py-4 flex justify-between items-center">
+      <div className="  max-w-7xl  mx-auto px-4 py-4 flex justify-between items-center h-16"> {/*Add a fixed height h-16 */}
         <Link href="/main" className="text-3xl font-bold flex gap-2 text-white">
-        <Image src={Logo} alt="Logo" width={40} className="rounded-md" height={40} /> 
-          Hive<span className="text-primary">Hub</span>
+        <Image src={Logo} alt="Logo" width={200} className="rounded-md" height={40} /> 
+          {/*<span className="text-primary">Aivora</span>*/}
         </Link>
         <nav className="space-x-6">
           <Link href="#features" className="text-white hover:text-primary transition-colors">
